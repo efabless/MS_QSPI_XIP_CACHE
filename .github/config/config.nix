9fd864ff2372c7bc2f28988fcce0133e3351671f
Kareem Farid <kareefardi@users.noreply.github.com>
2024-04-03 22:22:31 +0200

    Remove unneeeded things

--- .github/config/config.nix
+++
@@ -1,3 +0,0 @@
-extra-experimental-features = nix-command flakes
-extra-substituters = https://openlane.cachix.org
-extra-trusted-public-keys = openlane.cachix.org-1:qqdwh+QMNGmZAuyeQJTH9ErW57OWSvdtuwfBKdS254E=
--- .github/scripts/install-nix.bash
+++
@@ -1,1 +0,0 @@
-sh <(curl -L https://nixos.org/nix/install) --yes --daemon --nix-extra-conf-file ./.github/config/config.nix
