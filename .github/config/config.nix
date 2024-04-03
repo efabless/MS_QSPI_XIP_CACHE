3073a53a23ecaf0509792ff7f1b1f05fb05be916
Kareem Farid <kareefardi@users.noreply.github.com>
2024-04-03 21:56:33 +0200

    don't rely on heredoc

---
+++ .github/config/config.nix
@@ -0,0 +1,3 @@
+extra-experimental-features = nix-command flakes
+extra-substituters = https://openlane.cachix.org
+extra-trusted-public-keys = openlane.cachix.org-1:qqdwh+QMNGmZAuyeQJTH9ErW57OWSvdtuwfBKdS254E=
--- .github/scripts/install-nix.bash
+++ .github/scripts/install-nix.bash
@@ -1,5 +1,1 @@
-  sh -c <(curl -L https://nixos.org/nix/install) --yes --daemon --nix-extra-conf-file /dev/stdin <<EXTRA_NIX_CONF
-  extra-experimental-features = nix-command flakes
-  extra-substituters = https://openlane.cachix.org
-  extra-trusted-public-keys = openlane.cachix.org-1:qqdwh+QMNGmZAuyeQJTH9ErW57OWSvdtuwfBKdS254E=
-  EXTRA_NIX_CONF
+sh -c <(curl -L https://nixos.org/nix/install) --yes --daemon --nix-extra-conf-file ./.github/config/config.nix
